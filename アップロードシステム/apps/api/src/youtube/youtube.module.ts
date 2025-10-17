49fc2b041353c8d5de2787579276d41403e19d88
Kosuke Shibayama <advalay.company@gmail.com>
2025-10-17 15:20:22 +0900

    fix: Amplify設定を修正 - 静的エクスポート対応

--- アップロードシステム/apps/api/src/youtube/youtube.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { YoutubeService } from './youtube.service';
-
-@Global()
-@Module({
-  providers: [YoutubeService],
-  exports: [YoutubeService],
-})
-export class YoutubeModule {}
